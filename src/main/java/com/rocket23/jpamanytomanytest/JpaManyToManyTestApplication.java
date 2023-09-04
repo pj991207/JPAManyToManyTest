369712f7b4a9c4e453c87f858f87d2bd981ba097
pj991207 <pj991207@gmail.com>
2023-09-04 14:41:07 +0900

    feat: @EnableJpaAuditing 추가 및 수정날짜 생성날짜 기록

--- src/main/java/com/rocket23/jpamanytomanytest/JpaManyToManyTestApplication.java
+++ src/main/java/com/rocket23/jpamanytomanytest/JpaManyToManyTestApplication.java
@@ -2,8 +2,10 @@ package com.rocket23.jpamanytomanytest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JpaManyToManyTestApplication {
 
 	public static void main(String[] args) {
