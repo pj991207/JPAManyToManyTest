9a9f9c3e5320ec7eddee6c24859414fd08ba07fb
pj991207 <pj991207@gmail.com>
2023-09-04 10:32:07 +0900

    feat : jpa many to many test

---
+++ src/main/java/com/rocket23/jpamanytomanytest/JpaManyToManyTestApplication.java
@@ -0,0 +1,13 @@
+package com.rocket23.jpamanytomanytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaManyToManyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaManyToManyTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocket23/jpamanytomanytest/JpaManyToManyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocket23.jpamanytomanytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaManyToManyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
